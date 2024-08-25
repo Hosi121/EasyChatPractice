8314620d7deb43c2cab2b429e53198b8ff913dee
Hosi121 <takuyakataiwa@icloud.com>
2024-08-25 21:35:27 +0900

    fix: import関連のミスを修正

--- EasyChat/internal/repository/ent/schema/chat.go
+++ EasyChat/internal/repository/ent/schema/chat.go
@@ -1,28 +1,29 @@
 package schema
 
 import (
-    "entgo.io/ent"
-    "entgo.io/ent/schema/field"
-    "entgo.io/ent/schema/edge"
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
 )
 
 // Chat holds the schema definition for the Chat entity.
 type Chat struct {
-    ent.Schema
+	ent.Schema
 }
 
 // Fields of the Chat.
 func (Chat) Fields() []ent.Field {
-    return []ent.Field{
-        field.Time("created_at").Default(time.Now),
-    }
+	return []ent.Field{
+		field.Time("created_at").Default(time.Now),
+	}
 }
 
 // Edges of the Chat.
 func (Chat) Edges() []ent.Edge {
-    return []ent.Edge{
-        edge.From("users", User.Type).Ref("chats").Unique(),
-        edge.To("messages", Message.Type),
-    }
+	return []ent.Edge{
+		edge.From("users", User.Type).Ref("chats").Unique(),
+		edge.To("messages", Message.Type),
+	}
 }
-
--- EasyChat/internal/repository/ent/schema/group.go
+++ EasyChat/internal/repository/ent/schema/group.go
@@ -2,29 +2,30 @@
 package schema
 
 import (
-    "entgo.io/ent"
-    "entgo.io/ent/schema/field"
-    "entgo.io/ent/schema/edge"
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
 )
 
 // Group holds the schema definition for the Group entity.
 type Group struct {
-    ent.Schema
+	ent.Schema
 }
 
 // Fields of the Group.
 func (Group) Fields() []ent.Field {
-    return []ent.Field{
-        field.String("name").NotEmpty(),
-        field.Time("created_at").Default(time.Now),
-    }
+	return []ent.Field{
+		field.String("name").NotEmpty(),
+		field.Time("created_at").Default(time.Now),
+	}
 }
 
 // Edges of the Group.
 func (Group) Edges() []ent.Edge {
-    return []ent.Edge{
-        edge.From("users", User.Type).Ref("groups"),
-        edge.To("messages", Message.Type),
-    }
+	return []ent.Edge{
+		edge.From("users", User.Type).Ref("groups"),
+		edge.To("messages", Message.Type),
+	}
 }
-
